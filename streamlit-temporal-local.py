c687133f77982b7ed671edb70f05e02f34d2cdd8
vivszan <szaniszlov1@southernct.edu>
2024-11-20 14:26:56 -0500

    Update streamlit-temporal-local.py

--- streamlit-temporal-local.py
+++ streamlit-temporal-local.py
@@ -10,7 +10,7 @@ bridge = pd.read_csv(filename,
                      parse_dates=True,
                      index_col='hour_beginning')
 
-st.subheader('Brooklyn Bridge Pedestrian Crossings')
+st.subheader('Brooklyn Bridge Pedestrian Crossings Vivien WOO')
 st.markdown('---')
 volume = st.radio("Pedestrian Traffic Volume",('Hourly', 'Daily', 'Weekly'))
 st.markdown('---')
